fefec3f334c1f85727e090f334111d52361bc887
Brent Schmidt <bschmidt@oreilly.com>
2023-11-30 11:05:47 -0500

    feat: Add manifest request

--- v17/index.mjs
+++ v17/index.mjs
@@ -1,10 +1,15 @@
 import pacote from 'pacote';
 
 async function main() {
-  console.time('request');
+  console.time('request - manifest');
+  const manifest = await pacote.manifest('@colors/colors@1.6.0', { registry: 'https://nexus.common-build.gcp.oreilly.com/repository/npm-group/' });
+  console.log('got manifest for ' + manifest.name + '@' + manifest.version);
+  console.timeEnd('request - manifest');
+
+  console.time('request - tarball');
   const data = await pacote.tarball('https://nexus.common-build.gcp.oreilly.com/repository/npm-group/@colors/colors/-/colors-1.6.0.tgz');
   console.log('got ' + data.length + ' bytes of tarball data');
-  console.timeEnd('request');
+  console.timeEnd('request - tarball');
 }
 
 main();
